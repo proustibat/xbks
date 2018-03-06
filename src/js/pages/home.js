04d36401f0703dc97d81e0e9de0195bbcda7ebcf
Jennifer Proust <jennifer.proust@gmail.com>
2018-03-06 16:50:52 +0100

    feat(Api Service): get all books method

--- src/js/pages/home.js
+++ src/js/pages/home.js
@@ -1,13 +1,20 @@
 import Layout from '../layout';
+import ApiPotier from '../services/api-potier';
 
 export default class Home {
     constructor ( el ) {
         console.log( 'Hello Home' );
         this.container = el;
+        this.loader = null;
         this.layout = null;
+        this.api = new ApiPotier();
+        this.books = null;
+
+        this.createMainLoader();
 
         try {
             this.showPage();
+            this.runApp();
         }
         catch ( e ) {
             console.error( `Something's wrong :( ${ e }` );
@@ -19,8 +26,30 @@ export default class Home {
             throw new Error( 'This app must be wrapped in a dom element with a ".main-container" class!' );
         }
         else {
-            this.layout = new Layout();
             this.container.classList.add( 'complete' );
         }
+    }
+
+    async runApp () {
+        this.layout = new Layout();
+        this.displayMainLoader();
+        await this.api.getAllBooks().then( data => { this.books = data; } );
+        this.removeMainLoader();
+    }
+
+    createMainLoader () {
+        this.loader = document.createElement( 'div' );
+        this.loader.setAttribute( 'class', 'main-loader progress' );
+        const contentLoader = document.createElement( 'div' );
+        contentLoader.setAttribute( 'class', 'indeterminate' );
+        this.loader.appendChild( contentLoader );
+    }
+
+    displayMainLoader () {
+        this.container.prepend( this.loader );
+    }
+
+    removeMainLoader () {
+        this.loader.remove();
     }
 }
---
+++ src/js/services/api-potier.js
@@ -0,0 +1,33 @@
+import 'jquery'; // As Materialize-css requires jQuery, I use it also for http request
+
+let instance = null;
+
+class ApiPotier {
+    constructor () {
+        if ( !instance ) {
+            instance = this;
+        }
+        this.urlAllBooks = 'http://henri-potier.xebia.fr/books';
+        return instance;
+    }
+
+    getAllBooks () {
+        return new Promise( ( resolve, reject ) => {
+            // TODO: remove timeout, here just to test promise and loader display
+            setTimeout( () => {
+                $.ajax( {
+                    url: this.urlAllBooks,
+                    type: 'GET',
+                    success: resolve,
+                    error: reject
+                } );
+            }, 5000 );
+        } );
+    }
+
+    urlDiscount ( isbnList = [] ) {
+        return `http://henri-potier.xebia.fr/books/${ isbnList.join( ',' ) }/commercialOffers`;
+    }
+}
+
+export default ApiPotier;
